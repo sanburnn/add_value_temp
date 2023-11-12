d31e4cddc51643e67457a8517a5cc8b86a742cd0
sanburnn <dimas00758@gmail.com>
2023-11-12 22:21:36 +0700

    read per line

--- engine.py
+++ engine.py
@@ -1,13 +1,36 @@
-import pandas as pd
 
-# Membaca file Excel
-file_path = '/Users/dimas/Downloads/test.xls'  # Ganti dengan path file Excel yang sesuai
-df = pd.read_excel(file_path)
+file_path = '/Users/dimas/Downloads/test2.xls'  # Replace with the path to your text file
 
-# Menambahkan nilai 0.5 ke semua nilai di kolom "Temp.(°C)"
-df['Temp.(°C)'] = df['Temp.(°C)'] + 0.5
+# Try different encodings to read the file
+encodings_to_try = ['utf-8', 'latin-1', 'utf-16']
 
-# Menyimpan perubahan kembali ke file Excel
-df.to_excel(file_path, index=False)
+lines = None
+for encoding in encodings_to_try:
+    try:
+        with open(file_path, 'r', encoding=encoding) as file:
+            lines = file.readlines()
+        break  # Stop trying encodings if successful
+    except UnicodeDecodeError:
+        pass
 
-print("Nilai 0.5 telah ditambahkan ke semua nilai di kolom 'Temp.(°C)'.")
+if lines is None:
+    raise ValueError("Unable to determine the correct encoding for the file.")
+
+# Process the data
+for i in range(1, len(lines)):
+    values = lines[i].split()  # Assuming space or tab as the delimiter
+    if len(values) >= 3:  # Make sure the third column exists
+        try:
+            # Convert the third column value to float and add 0.5
+            values[2] = str(float(values[2]) + 0.5)
+            lines[i] = ' '.join(values)
+
+        except ValueError:
+            # Handle the case where the value in the third column is not a valid float
+            pass
+
+# Write the modified data back to the text file
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.writelines(lines)
+
+print("0.5 has been added to the values in the third column starting from the second row in the text file.")
